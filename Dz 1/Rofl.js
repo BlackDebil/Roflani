80f4db19106b67814ed7117432fe16abdfb46e28
BlackDebil <kononovih01@gmail.com>
2021-02-03 11:24:30 +0300

    Update Rofl.js

--- Dz 1/Rofl.js
+++ Dz 1/Rofl.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function () {
     let but1 = document.getElementById("but1");
     let but2 = document.getElementById("but2");
     but1.addEventListener('click', function (){
-      window.location.href = "https://www.youtube.com/watch?v=f709fmeZZ1A";
+      window.location.href = "https://mangalib.me/bleach?section=chapters";
     });
     but2.addEventListener('click', function(){
       document.body.style.backgroundColor = "purple";
