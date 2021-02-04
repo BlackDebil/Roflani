b7b782fb93ac2e44fe13a5dcad0f94194d33f6d3
BlackDebil <kononovih01@gmail.com>
2021-02-04 14:28:51 +0300

    Roflani

--- Dz 1/Rofl.js
+++ Dz 1/Rofl.js
@@ -5,22 +5,26 @@ window.addEventListener('load', function () {
       window.location.href = "https://mangalib.me/bleach?section=chapters";
     });
     but2.addEventListener('click', function(){
+		var audio = new Audio(); 
+	 audio.src = 'mocart.mp3'; 
+	 audio.autoplay = true; 
+		
 		but1.style.display = "none";
 		but2.style.display = "none";
 	  let img = document.createElement("img")
 	  img.src = "leus.jpg";
 	  img.style.align = "center";
 	  alert("ALARM! ALARM! CREATE LEUS!");
-	  document.body.appendChild(img)	
-	
-let timerId = setInterval(() =>  { 
+	  document.body.appendChild(img)
+	  
+setInterval(() =>  { 
   document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 }, 1);
 
 	  img.animate([
 	  { transform: 'rotate(360deg)'}
 	  ],{
-		duration: 1,
+		duration: 500,
 		iterations: Infinity
 	  });
 	  
